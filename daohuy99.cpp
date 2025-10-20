8b3c9d503d19ff859e37c72672d110f52d258702
daohuy99 <daoxuanhuy21092007@gmail.com>
2025-10-21 01:19:27 +0700

    Add files via upload

---
+++ daohuy99.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+    float doamdat;
+    float doamkk;
+    float nhietdo;
+    printf("He Thong Tuoi Tieu Tu Dong\n");
+    while(1){
+    printf("\nNhap du lieu thoi tiet:\n");    
+    printf("do am cua dat (%) la: ");
+    scanf("%f",&doamdat);
+    printf("do am cua khong khi(%) la:");
+    scanf("%f",&doamkk);
+    printf("nhiet do cua dat (°C) la:");
+    scanf("%f",&nhietdo);
+     if (doamdat < 0 || doamdat > 100 || doamkk < 0 || doamkk > 100 || nhietdo < -10 || nhietdo > 60) {
+            printf(" Du lieu khong hop le!\n");
+            continue;
+        }
+    printf("\n Ket qua phan tich:\n");
+    if(doamkk>80){
+        printf("troi mua -> he thong tat bom \n ");}
+        else if(nhietdo>=30 && doamkk>50 && doamdat<40){
+            printf("troi nang -> he thong bat bom trong 5s \n");
+            sleep(5);
+        }
+        else if(doamdat<35 ){
+printf("troi ram -> he thong bat bom trong 5s \n ");
+sleep(5);
+    }else {
+        printf("dat du am -> tat bom \n");
+    }}}
